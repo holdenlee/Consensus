3c8369b1c4f270cfc53bdf510d98b77b5ce89107
Holden Lee <holdenl@princeton.edu>
2014-11-24 16:00:26 -0500

    get proposals

--- CompliantNode.java
+++ CompliantNode.java
@@ -8,6 +8,7 @@ public class CompliantNode implements Node {
 
 	private int numFs;
 	private HashMap<Integer, Integer> votes;
+	private int rounds;
 	
 	public void addVote(int id) {
 		Integer id2=new Integer(id);
@@ -22,10 +23,12 @@ public class CompliantNode implements Node {
 			double p_txDistribution, int numRounds) {
 		// TODO Auto-generated constructor stub
 		numFs=0;
+		rounds=0;
 	}
 
 	//id of tx, then id of broadcaster
 	public void receiveCandidates(ArrayList<Integer[]> candidates) {
+		numFs++;
 		for (Integer[] list: candidates) {
 			for (int i=0;i<list.length;i++){
 				addVote(list[0]);
@@ -34,8 +37,14 @@ public class CompliantNode implements Node {
 	}
 
 	public Set<Transaction> getProposals() {
+		Set<Transaction> txs = new HashSet<Transaction>();
 		//loop through and send off majorities.
-		return new HashSet<Transaction>();
+		for (Integer i: votes.keySet()) {
+			if (votes.get(i)>(numFs/2.0)) {
+				txs.add(new Transaction(votes.get(i)));
+			}
+		}
+		return txs;
 	}
 
 	public void setFollowees(boolean[] followees) {
